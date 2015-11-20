7bd7fae225487604e939fedce07e60dbf5bbbbd3
Robert M Ochshorn <rmo@NUMM.ORG>
2015-11-20 11:31:32 -0800

    remove numpy dependency, and install twisted on ubuntu

--- dependencies_osx.sh
+++ dependencies_osx.sh
@@ -1,4 +1,4 @@
 sudo easy_install pip
-sudo pip install numpy Pillow
+sudo pip install Pillow
 
 brew install ffmpeg libtool automake autoconf subversion wget
--- dependencies_ubuntu.sh
+++ dependencies_ubuntu.sh
@@ -1,5 +1,6 @@
 sudo apt-get install -y zlib1g-dev automake autoconf git libtool subversion \
-     libatlas3-base ffmpeg python-numpy python-imaging
+     libatlas3-base ffmpeg python-twisted-web
+     
 
 # Kaldi seems to depend on this... 
 sudo ln -s -f bash /bin/sh
