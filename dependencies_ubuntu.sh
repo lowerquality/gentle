40e43c8007012f032eedec98827d06a7ddaf06aa
Max Hawkins <maxhawkins@gmail.com>
2015-11-23 13:51:27 +0100

    Remove scary /bin/sh overwrite
    We don't rely on shell scripts anymore. And I'd be really pissed if someone overwrote my /bin/sh without asking!

--- dependencies_ubuntu.sh
+++ dependencies_ubuntu.sh
@@ -1,7 +1,4 @@
-sudo apt-get install -y zlib1g-dev automake autoconf git libtool subversion \
-     libatlas3-base ffmpeg python-pip
-     
-# Kaldi seems to depend on this... 
-sudo ln -s -f bash /bin/sh
+sudo apt-get install -y zlib1g-dev automake autoconf git \
+	libtool subversion libatlas3-base ffmpeg python-pip
 
 sudo pip install .
