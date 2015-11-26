d9fd97943921ba31f3f9d1a8ac06915b0f918ffe
Max Hawkins <maxhawkins@gmail.com>
2015-11-26 23:09:05 +0100

    reduce duplication: generate_word_sequence isn't used anywhere

--- gentle/generate_wp.py
+++ gentle/generate_wp.py
@@ -4,29 +4,7 @@ import sys
 import re
 import math
 
-def generate_word_sequence(words_file, transcript_file):
-    # XXX: If we refactored as a class, we could avoid repeatedly
-    # loading the vocabulary
-    vocabulary = set([X.split(' ')[0] for X in open(words_file).read().split('\n')])
-
-    # Despite ARPA being all-caps, we may need lowercase.
-    transcript = open(transcript_file).read().lower()
-
-    # Turn hyphens into spaces
-    transcript = transcript.replace('-', ' ')
-
-    # Get rid of all punctuation except for periods and spaces
-    # XXX: getting rid of periods
-    transcript = re.sub(r'[^a-z\s\']', '', transcript)
-
-    # # Replace periods with the SENTENCE-END sentinel
-    # transcript = transcript.replace(".", " . ")
-
-    word_sequence = [X.strip() for X in transcript.split() if len(X.strip()) > 0 and not X.startswith("'")]
-
-    # We need to limit to words within a vocabulary
-    word_sequence = [X if X in vocabulary else '[oov]' for X in word_sequence]
-    return word_sequence
+from metasentence import MetaSentence
 
 def language_model_from_word_sequence(word_sequence):
     word_sequence = ['[oov]', '[oov]'] + word_sequence + ['[oov]']
@@ -64,7 +42,6 @@ def language_model_from_word_sequence(word_sequence):
 
 
 if __name__=='__main__':
-
     USAGE = "python generate_wp.py WORDS.TXT TRANSCRIPT OUTPUT_FILE"
     if len(sys.argv) != 4:
         print USAGE
@@ -74,6 +51,10 @@ if __name__=='__main__':
     TRANSCRIPT_FILE = sys.argv[2]
     OUTPUT_FILE = sys.argv[3]
 
-    word_sequence = generate_word_sequence(WORDS_FILE, TRANSCRIPT_FILE)
+    vocabulary = set([X.split(' ')[0] for X in open(WORDS_FILE).read().split('\n')])
+    transcript = open(TRANSCRIPT_FILE).read()
+
+    ms = MetaSentence(transcript, vocabulary)
+    word_sequence = ms.get_kaldi_sequence()
     lm = language_model_from_word_sequence(word_sequence)
     open(OUTPUT_FILE, 'w').write(lm)
