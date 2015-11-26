2374d98d2d39784e9b54770261c02219112c40c9
Robert M Ochshorn <rmo@NUMM.ORG>
2015-11-25 20:16:06 -0800

    rudimentary GUI

--- gentle.py
+++ gentle.py
@@ -1,6 +1,7 @@
 import logging
 import threading
 from twisted.internet import reactor
+import webbrowser
 import wx
 
 from gentle.paths import get_resource, get_datadir
@@ -10,7 +11,13 @@ class MainWindow(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(200,100))
 
-        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        panel = wx.Panel(self)
+        self.browse = wx.Button(panel, label="Open in web browser", pos=(0, 0))
+        self.Bind(wx.EVT_BUTTON, self.OnBrowseClick, self.browse)
+
+        self.quitb = wx.Button(panel, label="Quit", pos=(0, 30))
+        self.Bind(wx.EVT_BUTTON, self.OnClose, self.quitb)
+
         self.CreateStatusBar()
 
         # Setting up the menu.
@@ -40,12 +47,20 @@ class MainWindow(wx.Frame):
     def OnAbout(self, event):
         logging.info("clicked on ABOUT")
 
+        # Create a little modal dialog box
+        dlg = wx.MessageDialog(self, "Robust yet lenient forced aligner", "Gentle", wx.OK)
+        dlg.ShowModal()
+        dlg.Destroy()
+
     def OnClose(self, event):
         logging.info("Quitting gentle.")
         reactor.callFromThread(reactor.stop)
         logging.info("Waiting for server to quit.")
         self.webthread.join()
         self.Destroy()
+
+    def OnBrowseClick(self, event):
+        webbrowser.open("http://localhost:8765")
 
 app = wx.App(False)
 frame = MainWindow(None, "Gentle")
