34ecfb622fc5678313182046fbbc420c916e0e46
Max Hawkins <maxhawkins@gmail.com>
2015-11-26 14:11:45 +0100

    serve.py: allow setting log level

--- serve.py
+++ serve.py
@@ -157,8 +157,13 @@ if __name__=='__main__':
                        help='host to run http server on')
     parser.add_argument('--port', default=8080, type=int,
                         help='port number to run http server on')
+    parser.add_argument('--log', default="WARNING",
+                        help='the log level (DEBUG, INFO, WARNING, ERROR, or CRITICAL)')
 
     args = parser.parse_args()
+
+    log_level = args.log.upper()
+    logging.basicConfig(level=log_level)
 
     print 'listening at %s:%d\n' % (args.host, args.port)
 
