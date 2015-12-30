0d6f8102af29621c54a44eb4396d6e0ebe56cf07
Robert M Ochshorn <rmo@NUMM.ORG>
2015-12-30 15:57:03 -0800

    Implement zipfile download.
    On first request to the new /zip endpoint, the transcription directory is generated.
    It is cached for later requests.
    
    view_alignment.html is modified with INLINE_JSON after the transcription has
    completed so that the ZIP bundle can run from file:/// domains.

--- serve.py
+++ serve.py
@@ -16,6 +16,7 @@ import uuid
 from gentle.paths import get_binary, get_resource, get_datadir
 from gentle.language_model_transcribe import lm_transcribe_progress
 from gentle.transcription import to_csv
+from gentle.cyst import Insist
 import gentle
 
 # The `ffmpeg.to_wav` function doesn't set headers properly for web
@@ -57,7 +58,7 @@ class Transcriber():
         outdir = os.path.join(self.data_dir, 'transcriptions', uid)
         os.makedirs(outdir)
 
-        # Finally, copy over the HTML
+        # Copy over the HTML
         shutil.copy(get_resource('www/view_alignment.html'), os.path.join(outdir, 'index.html'))
 
         tran_path = os.path.join(outdir, 'transcript.txt')
@@ -99,6 +100,11 @@ class Transcriber():
 
         output['status'] = 'OK'
         save()
+
+        # Inline the alignment into the index.html file.
+        htmltxt = open(get_resource('www/view_alignment.html')).read()
+        htmltxt = htmltxt.replace("var INLINE_JSON;", "var INLINE_JSON=%s;" % (json.dumps(output)));
+        open(os.path.join(outdir, 'index.html'), 'w').write(htmltxt)
 
         logging.info('done with transcription.')
 
@@ -147,11 +153,46 @@ class TranscriptionsController(Resource):
         req.setHeader(b"Location", "/transcriptions/%s" % (uid))
         return ''
 
+class LazyZipper(Insist):
+    def __init__(self, cachedir, transcriber, uid):
+        self.transcriber = transcriber
+        self.uid = uid
+        Insist.__init__(self, os.path.join(cachedir, '%s.zip' % (uid)))
+
+    def serialize_computation(self, outpath):
+        shutil.make_archive('.'.join(outpath.split('.')[:-1]), # We need to strip the ".zip" from the end
+                            "zip",                             # ...because `shutil.make_archive` adds it back
+                            os.path.join(self.transcriber.out_dir(self.uid)))
+
+class TranscriptionZipper(Resource):
+    def __init__(self, cachedir, transcriber):
+        self.cachedir = cachedir
+        self.transcriber = transcriber
+        Resource.__init__(self)
+    
+    def getChild(self, path, req):
+        print 'tz path', path
+        uid = path.split('.')[0]
+        t_dir = self.transcriber.out_dir(uid)
+        if os.path.exists(t_dir):
+            # TODO: Check that "status" is complete and only create a LazyZipper if so
+            # Otherwise, we could have incomplete transcriptions that get permanently zipped.
+            # For now, a solution will be hiding the button in the client until it's done.
+            lz = LazyZipper(self.cachedir, self.transcriber, uid)
+            self.putChild(path, lz)
+            return lz
+        else:
+            return Resource.getChild(self, path, req)
+
 def serve(port=8765, interface='0.0.0.0', installSignalHandlers=0, data_dir=get_datadir('webdata')):
     logging.info("SERVE %d, %s, %d", port, interface, installSignalHandlers)
     
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
+
+    zip_dir = os.path.join(data_dir, 'zip')
+    if not os.path.exists(zip_dir):
+        os.makedirs(zip_dir)
     
     f = File(data_dir)
 
@@ -162,6 +203,9 @@ def serve(port=8765, interface='0.0.0.0', installSignalHandlers=0, data_dir=get_
     trans = Transcriber(data_dir)
     trans_ctrl = TranscriptionsController(trans)
     f.putChild('transcriptions', trans_ctrl)
+
+    trans_zippr = TranscriptionZipper(zip_dir, trans)
+    f.putChild('zip', trans_zippr)
     
     s = Site(f)
     logging.info("about to listen")
