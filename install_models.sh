d546200bb73454bda177b73b72c52650397dc6a1
Robert M Ochshorn <rmo@NUMM.ORG>
2015-12-30 20:27:43 -0800

    download models over https

--- install_models.sh
+++ install_models.sh
@@ -7,7 +7,7 @@ VERSION="0.02"
 download_models() {
 	local version="$1"
 	local filename="kaldi-models-$version.zip"
-	local url="http://lowerquality.com/gentle/$filename"
+	local url="https://lowerquality.com/gentle/$filename"
 	wget -O $filename $url
 	unzip $filename
 }
