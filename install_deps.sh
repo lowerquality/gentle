240ede918b94836d1b490624f800849859b81a13
j <j@mailb.org>
2016-08-12 16:54:22 +0200

    Don't fail if ffmpeg is not in repository
    On Ubuntu 14.04 ffmpeg is not available in the default repository.
    In that case, print a warning and install the remaining dependencies.

--- install_deps.sh
+++ install_deps.sh
@@ -6,11 +6,11 @@ echo "Installing dependencies..."
 
 # Install OS-specific dependencies
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	apt-get update
+	apt-get update -qq
 	apt-get install -y zlib1g-dev automake autoconf git \
-		libtool subversion libatlas3-base ffmpeg python-pip \
+		libtool subversion libatlas3-base python-pip \
 		python-dev wget unzip
-
+	apt-get install -y ffmpeg || echo -n  "\n\nYou have to install ffmpeg from a PPA or from https://ffmpeg.org before you can run gentle\n\n"
 	pip install .
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	brew install ffmpeg libtool automake autoconf wget
