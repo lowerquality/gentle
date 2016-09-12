af407bc187f8955963025cb45644671df3d02d33
ronen barzel <ronen@barzel.org>
2016-09-12 10:01:45 +0100

    resource locations specified in environment variable; by default looks in project root

--- gentle/resources.py
+++ gentle/resources.py
@@ -1,6 +1,7 @@
+import logging
 import os
 
-from util.paths import get_resource
+from util.paths import get_resource, ENV_VAR
 from gentle import metasentence
 
 class Resources():
@@ -9,5 +10,16 @@ class Resources():
         self.proto_langdir = get_resource('PROTO_LANGDIR')
         self.nnet_gpu_path = get_resource('data/nnet_a_gpu_online')
         self.full_hclg_path = get_resource('data/graph/HCLG.fst')
+
+        def require_dir(path):
+            if not os.path.isdir(path):
+                raise RuntimeError("No resource directory %s.  Check %s environment variable?" % (path, ENV_VAR))
+
+
+        require_dir(self.proto_langdir)
+        require_dir(self.nnet_gpu_path)
+
         with open(os.path.join(self.proto_langdir, "graphdir/words.txt")) as fh:
             self.vocab = metasentence.load_vocabulary(fh)
+
+
