014cf212b1fea788fe251eb40f69ba81ae5000b2
Robert M Ochshorn <mail@RMOZONE.COM>
2017-01-24 17:59:16 +0100

    fix language model url

--- install_language_model.sh
+++ install_language_model.sh
@@ -1,3 +1,3 @@
-wget -c /lockers/interlace_vol/lowerquality.com/gentle/aspire-hclg.tar.gz
+wget -c https://lowerquality.com/gentle/aspire-hclg.tar.gz
 tar -xzvf aspire-hclg.tar.gz
 rm aspire-hclg.tar.gz
