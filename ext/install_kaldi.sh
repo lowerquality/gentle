0d961794243a14cda4424292cdd13b16344d404a
Robert M Ochshorn <mail@RMOZONE.COM>
2017-02-12 16:19:15 -0500

    modify install_kaldi script to clean kaldi directory (closes #127)

--- ext/install_kaldi.sh
+++ ext/install_kaldi.sh
@@ -2,8 +2,10 @@
 
 # Prepare Kaldi
 cd kaldi/tools
+make clean
 make atlas openfst OPENFST_VERSION=1.4.1
 cd ../src
+make clean
 ./configure --static --static-math=yes --static-fst=yes --use-cuda=no
 make depend
 cd ../../
