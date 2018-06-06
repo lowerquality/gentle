61167690b906a2478087ab3614d4421027c39eec
tlc <troycauble@gmail.com>
2018-06-06 13:08:02 -0400

    Forgot gfortran.

--- install_deps.sh
+++ install_deps.sh
@@ -22,14 +22,14 @@ elif [[ "$OSTYPE" == "linux-gnu" ]]; then
 		$SUDO apt-get update -qq
 		$SUDO apt-get install -y zlib1g-dev automake autoconf git \
 			libtool subversion libatlas3-base python-pip \
-			python-dev wget unzip python3
+			python-dev wget unzip gfortran python3
 		$SUDO apt-get install -y ffmpeg || echo -n  "\n\nYou have to install ffmpeg from a PPA or from https://ffmpeg.org before you can run gentle\n\n"
 		$SUDO pip install -e .
 	elif [[ "$ID" == "centos" ]]; then
 		sudo yum check-update
 		sudo yum install -y zlib1g-dev automake autoconf git \
 			libtool subversion libatlas3-base python-pip \
-			python-dev wget unzip python34 python34-pip
+			python-dev wget unzip gcc-gfortran python34 python34-pip
 		sudo yum install -y ffmpeg || echo -n  "\n\nYou have to install ffmpeg from a PPA or from https://ffmpeg.org before you can run gentle\n\n"
 		sudo pip install -e .    # NOT pip3 !!
 	fi
