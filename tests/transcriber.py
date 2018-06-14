2ace7695fe214782245d2cbbe28d060123aba42e
j <j@mailb.org>
2018-06-14 11:53:52 +0200

    more tests
     - before running transcription test, make sure resouces are present
     - output errors from subprocess while running tests
     - cleanup travis config to get enough memory

--- tests/transcriber.py
+++ tests/transcriber.py
@@ -1,16 +1,31 @@
+import os
 import unittest
 
-
 class Transcriber(unittest.TestCase):
+    audio = 'examples/data/lucier.mp3'
+
+    def test_resources(self):
+        from gentle import Resources
+        from gentle.util.paths import get_binary
+
+        resources = Resources()
+        k3 = get_binary("ext/k3")
+
+        self.assertEqual(os.path.exists(resources.full_hclg_path), True)
+        self.assertEqual(os.path.exists(self.audio), True)
+        self.assertEqual(os.path.exists(k3), True)
 
     def test_transcriber(self):
+        import subprocess
         from gentle import resampled, kaldi_queue, standard_kaldi, Resources
         from gentle.transcriber import MultiThreadedTranscriber
+
+        standard_kaldi.STDERR = subprocess.STDOUT
 
         resources = Resources()
         k_queue = kaldi_queue.build(resources, 1)
         trans = MultiThreadedTranscriber(k_queue)
 
-        with resampled('examples/data/lucier.mp3', 10.5, 2.5) as filename:
+        with resampled(self.audio, 10.5, 2.5) as filename:
             words, duration = trans.transcribe(filename)
         self.assertEqual(words[0].word, "different")
