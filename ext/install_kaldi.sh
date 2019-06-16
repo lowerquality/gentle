a928cac4a96dd1510ca996926c44ea0ea74453aa
Robert M Ochshorn <mail@RMOZONE.COM>
2019-06-16 08:02:54 -0700

    'You shouldn't "make clean" before running "configure".' -dpovey

--- ext/install_kaldi.sh
+++ ext/install_kaldi.sh
@@ -6,7 +6,7 @@ make clean
 make
 ./extras/install_openblas.sh
 cd ../src
-make clean
+# make clean (sometimes helpful after upgrading upstream?)
 ./configure --static --static-math=yes --static-fst=yes --use-cuda=no --openblas-root=../tools/OpenBLAS/install
 make depend
 cd ../../
